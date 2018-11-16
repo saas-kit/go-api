2820cd3407a544259d05c6394fbbddb3302d78c6
Dmitry Momot <mail@dmomot.com>
2018-11-17 00:10:44 +0300

    JWT middleware with custom configuration

---
+++ delivery/http/jwt.go
@@ -0,0 +1,53 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+const (
+	// JWTContextKey is a key to save user data into context
+	JWTContextKey string = "JWT"
+)
+
+// JWTClaims is a custom JWT claims structure
+type JWTClaims struct {
+	Name  string `json:"name"`
+	Admin bool   `json:"admin"`
+	jwt.StandardClaims
+}
+
+// JWT Middleware
+func JWT(signingKey string) echo.MiddlewareFunc {
+	return middleware.JWTWithConfig(middleware.JWTConfig{
+		BeforeFunc:     beforeFunc,
+		SuccessHandler: successHandler,
+		ErrorHandler:   errorHandler,
+		SigningKey:     []byte(signingKey),
+		SigningMethod:  middleware.AlgorithmHS256,
+		ContextKey:     JWTContextKey,
+		Claims:         jwt.MapClaims{},
+		TokenLookup:    "header:" + echo.HeaderAuthorization,
+		AuthScheme:     "Bearer",
+	})
+}
+
+// Function runs before JWT middleware
+func beforeFunc(c echo.Context) {}
+
+// Successfull handler
+func successHandler(c echo.Context) {}
+
+// Error handler
+func errorHandler(err error) error {
+	if err == middleware.ErrJWTMissing {
+		return NewHTTPError(http.StatusUnauthorized, middleware.ErrJWTMissing.Message)
+	}
+	if err, ok := err.(*echo.HTTPError); ok {
+		return NewHTTPError(err.Code, err.Message)
+	}
+	return NewHTTPError(http.StatusUnauthorized, err)
+}
