6f30b7484e865b005fd3eab62f0bf76c6e4f3903
Dmitry Momot <mail@dmomot.com>
2018-11-17 00:08:18 +0300

    Custom structure binder with automatically request validation

---
+++ delivery/http/binder.go
@@ -0,0 +1,48 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/thedevsaddam/govalidator"
+)
+
+// Predefined errors
+var (
+	ErrMalformedJSON = echo.NewHTTPError(http.StatusBadRequest, "Malformed JSON")
+)
+
+type (
+	// Binder structure
+	Binder struct{}
+
+	// ValidatableStruct interface
+	ValidatableStruct interface {
+		ValidationRules() map[string][]string
+		ValidationMessages() map[string][]string
+	}
+)
+
+// Bind is a custom implementation of the `Binder#Bind` function.
+func (cb *Binder) Bind(i interface{}, c echo.Context) error {
+	if data, ok := i.(ValidatableStruct); ok {
+		v := govalidator.New(govalidator.Options{
+			Request:  c.Request(),
+			Data:     data,
+			Rules:    data.ValidationRules(),
+			Messages: data.ValidationMessages(),
+		})
+		if err := v.ValidateJSON(); len(err) > 0 {
+			if _, ok := err["_error"]; ok {
+				return ErrMalformedJSON
+			}
+			return echo.NewHTTPError(http.StatusBadRequest, err)
+		}
+	} else {
+		db := new(echo.DefaultBinder)
+		if err := db.Bind(i, c); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err)
+		}
+	}
+	return nil
+}
