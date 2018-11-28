3ce783855d91458a392ba40070c17416d569ea71
Dmitry Momot <mail@dmomot.com>
2018-11-29 00:32:50 +0300

    testing of address service

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"log"
-	"saas-kit-api/api/v1"
+	"saas-kit-api/app/address"
+	"saas-kit-api/pkg/config"
+	"saas-kit-api/pkg/database"
 	"saas-kit-api/pkg/server"
 )
 
 func main() {
+	// Init config
+	cnf := config.New()
+
+	// Create db connection
+	db, err := database.New(cnf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer checkErr(db.Close())
+
 	// Init new HTTP server
-	httpServer := server.New(config{})
-	// Set up API v1 services
-	// TODO: missed params
-	v1.SetUp(httpServer.Router())
+	httpServer := server.New(cnf)
+	router := httpServer.Router()
+
+	// Set up address microservice
+	checkErr(address.SetUp(cnf, router, db))
+
 	// Run web server via HTTP
 	log.Fatal(httpServer.ListenAndServe())
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
